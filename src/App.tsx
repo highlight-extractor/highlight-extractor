0f7a22fd53108b35bfacc05d5bd73d35eed2462d
ravi <sudha.rkj@gmail.com>
2019-12-12 03:54:19 -0800

    Add placeholders to view the images

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Upload from "./upload/Upload";
 import Highlights from "./highlights/Highlights";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 function Index() {
   return <h2>Home</h2>;
--- src/highlights/Highlights.tsx
+++ src/highlights/Highlights.tsx
@@ -1,9 +1,17 @@
-import { Component } from "react";
+import {Component, CSSProperties} from "react";
 import React from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 type Props = {}
 
 type State = { images: string[] }
+
+const defaults: CSSProperties = {
+    height: "auto",
+    width: "auto",
+    maxHeight: 350,
+    maxWidth: 350
+};
 
 class Highlights extends Component<Props, State> {
     constructor(props: Props) {
@@ -13,20 +21,26 @@ class Highlights extends Component<Props, State> {
                 "inside.png",
                 "outside.png"
             ]
-        }
+        };
     }
 
     renderImage(imageUrl: string): React.ReactNode {
+        console.log(imageUrl);
         return (
-            <img src={imageUrl} className="App-logo" alt="logo" />
+            <MDBCol md="4">
+                <img src={imageUrl} className="img-fluid hoverable" alt="logo" style={ defaults } />
+            </MDBCol>
         );
     }
 
     render(): React.ReactNode {
+        console.log(this.state);
         return (
-            <div>
-                {this.state.images.map(imageUrl =>  this.renderImage(imageUrl))}
-            </div>
+            <MDBContainer className="mt-5">
+                <MDBRow>
+                    {this.state.images.map(imageUrl =>  this.renderImage(imageUrl))}
+                </MDBRow>
+            </MDBContainer>
         );
     }
 }
